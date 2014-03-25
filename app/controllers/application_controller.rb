aff0936c6c4896a6865eeb5af07f0dacf5682278
sanya <sanya@sanya-netbook.(none)>
2014-03-25 10:22:55 +0400

    exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,18 @@ class ApplicationController < ActionController::Base
     return @current_section_title
   end
 
+ 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
+  end
+ 
+  private
+  def render_error(status, exception)
+    respond_to do |format|
+      format.html { render template: "share/error_#{status}.html.erb", layout: 'layouts/application', status: status }
+      format.all { render nothing: true, status: status }
+    end
+  end
+
 end
