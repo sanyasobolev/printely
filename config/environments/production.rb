817edd39861560fb1a67f1bc479e2ced595e8253
sanya <sanya@sanya.notebook>
2014-09-02 19:27:30 +0400

    v15

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Spsite::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -47,8 +47,8 @@ Spsite::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   
-  config.assets.precompile += %w( application.js )
-  config.assets.precompile = ['/assets/javascripts/extend/vendor/jquery.validate']
+  config.assets.precompile += [ 'application.js', 'rails.js'  ]
+  #config.assets.precompile = ['/assets/javascripts/extend/vendor/jquery.validate']
 
 
   # Disable delivery errors, bad email addresses will be ignored
