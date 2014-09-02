47c8fcb4445b7f84d0acb2d017c105c467d74bf5
sanya <sanya@sanya.notebook>
2014-09-02 09:02:42 +0400

    v13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Spsite::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -40,13 +40,13 @@ Spsite::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  # config.cache_stиore = :mem_cache_store
+  # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile = ['*.js', '*.css']
+   config.assets.precompile = ['application.js']
   #config.assets.precompile += %w( rails.js )
 
 
