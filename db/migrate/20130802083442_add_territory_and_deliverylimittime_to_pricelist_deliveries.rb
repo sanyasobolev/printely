fd56e8cc8587bfefe75206c641dc58b9efff977a
sanya <sanya@sanya.notebook>
2014-09-04 00:15:22 +0400

    v32

--- db/migrate/20130802083442_add_territory_and_deliverylimittime_to_pricelist_deliveries.rb
+++
@@ -1,6 +0,0 @@
-class AddTerritoryAndDeliverylimittimeToPricelistDeliveries < ActiveRecord::Migration
-  def change
-    add_column :pricelist_deliveries, :territory, :text
-    add_column :pricelist_deliveries, :delivery_limit_time, :string
-  end
-end
