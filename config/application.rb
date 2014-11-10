b90d86dde4f59fbfe2a09cb2705b3df5b0633b1e
sanya <sanya@sanya.notebook>
2014-11-11 00:48:26 +0300

    v15

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,3 @@
-# Unicorn self-process killer
-require 'unicorn/worker_killer'
-
-# Max requests per worker
-use Unicorn::WorkerKiller::MaxRequests, 3072, 4096
-
-# Max memory size (RSS) per worker
-use Unicorn::WorkerKiller::Oom, (192*(1024**2)), (256*(1024**2))
-
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
