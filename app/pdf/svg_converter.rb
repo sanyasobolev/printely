c021bb19e490e5dd46206f9dc80a8f490b6afffd
Sanya Sobolev <sanya@sanya.notebook>
2015-05-05 23:56:09 +0300

    v24

--- app/pdf/svg_converter.rb
+++ app/pdf/svg_converter.rb
@@ -4,7 +4,7 @@ class SvgConverter < Prawn::Document
   
   def to_pdf(order, canvas_settings)
 
-      font_families.update("Arial" => {
+      font_families.update('Arial', Helvetica, sans-serif => {
                                :normal => "#{Rails.root.to_s}/app/assets/fonts/arial.ttf",
                                :bold => "#{Rails.root.to_s}/app/assets/fonts/arial_bold.ttf",
                                :italic => "#{Rails.root.to_s}/app/assets/fonts/arial_italic.ttf",
