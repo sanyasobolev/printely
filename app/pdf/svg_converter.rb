e25723eb67a0cadbe9032d17f4948f9e58f1ef37
Sanya Sobolev <sanya@sanya.notebook>
2015-05-06 22:06:55 +0300

    v25

--- app/pdf/svg_converter.rb
+++ app/pdf/svg_converter.rb
@@ -4,24 +4,24 @@ class SvgConverter < Prawn::Document
   
   def to_pdf(order, canvas_settings)
 
-      font_families.update('Arial' => {
-                               :normal => "#{Rails.root.to_s}/app/assets/fonts/arial_prawn.ttf",
-                               :bold => "#{Rails.root.to_s}/app/assets/fonts/arial_bold.ttf",
-                               :italic => "#{Rails.root.to_s}/app/assets/fonts/arial_italic.ttf",
-                               :bold_italic => "#{Rails.root.to_s}/app/assets/fonts/arial_bold_italic.ttf" 
+      font_families.update("Arial" => {
+                               :normal => "#{Rails.root.to_s}/app/assets/fonts/arial_full.ttf",
+                               :bold => "#{Rails.root.to_s}/app/assets/fonts/arial_bold_full.ttf",
+                               :italic => "#{Rails.root.to_s}/app/assets/fonts/arial_italic_full.ttf",
+                               :bold_italic => "#{Rails.root.to_s}/app/assets/fonts/arial_bold_italic_full.ttf" 
                                           })
+      font_families.update("Times New Roman" => {
+                               :normal => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_full.ttf",
+                               :bold => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_bold_full.ttf",
+                               :italic => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_italic_full.ttf",
+                               :bold_italic => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_bold_italic_full.ttf"                                                                            
+                                          }) 
       font_families.update("Courier New" => {
                                :normal => "#{Rails.root.to_s}/app/assets/fonts/courier_new.ttf"
                                           })
       font_families.update("Calibri" => {
                                :normal => "#{Rails.root.to_s}/app/assets/fonts/calibri.ttf"
                                           })                                          
-      font_families.update("Times New Roman" => {
-                               :normal => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman.ttf",
-                               :bold => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_bold.ttf",
-                               :italic => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_italic.ttf",
-                               :bold_italic => "#{Rails.root.to_s}/app/assets/fonts/times_new_roman_bold_italic.ttf"                                                                            
-                                          })    
       font_families.update("Asylbek" => {
                                :normal => "#{Rails.root.to_s}/app/assets/fonts/asylbek.ttf"
                                           })
