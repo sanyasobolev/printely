95c722674f5e35629269cb9cf095ca31944f1d1f
Sanya Sobolev <sanya@sanya.notebook>
2015-08-17 09:53:11 +0300

    исправление ошибки с section_on_page

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -1,2 +1,11 @@
 module Admin::PagesHelper
+  
+  def section_for_page #возвращает перечень всех созданных пользователем разделов
+    Section.find(:all, :conditions => "controller='no'")
+  end
+  
+  def subsection_for_page #возвращает перечень всех созданных пользователем подразделов
+    Subsection.find(:all, :conditions => "controller='no'")
+  end
+  
 end
